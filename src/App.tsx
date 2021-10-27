2531561766854596e8b7a6a39b486f07cbf78d89
mansur <olaomansur@gmail.com>
2021-10-27 15:42:23 +0100

    tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import React from 'react'
-import "./App.css"
+import "./index.css"
 import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
+import "./PrimaryButton"
+import PrimaryButton from './PrimaryButton';
 
 
 const App = () => {
   return (
-    <div className='container'>
+    <div className="max-w-sm mx-auto flex flex-col shadow-xl rounded-xl mt-10 items-center justify-center text-white uppercase font-medium" >
       timer
-      <CircularProgressbar value={60} text={`60%`} />
-    </div>
+      < CircularProgressbar value={60} text={`60%`
+      } />
+      < div >
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mt-5 mb-2 uppercase rounded">Start
+        </button>
+        <button className="bg-red-500 hover:bg-red-700 ml-2 text-white font-bold py-2 px-4 mt-5 mb-2 uppercase rounded">Stop
+        </button>
+      </div >
+    </div >
   )
 }
 
